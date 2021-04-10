8e62b35b83c5dbe52523a73ff6b5fdf7f8cd433d
Jon <jonvolk666@gmail.com>
2021-04-10 09:39:06 -0400

    First commit

---
+++ libopeninv/include/my_math.h
@@ -0,0 +1,16 @@
+#ifndef MY_MATH_H_INCLUDED
+#define MY_MATH_H_INCLUDED
+
+#define ABS(a)   ((a) < 0?(-a) : (a))
+#define MIN(a,b) ((a) < (b)?(a):(b))
+#define MAX(a,b) ((a) > (b)?(a):(b))
+#define RAMPUP(current, target, rate) ((target < current || (current + rate) > target) ? target : current + rate)
+#define RAMPDOWN(current, target, rate) ((target > current || (current - rate) < target) ? target : current - rate)
+#define IIRFILTER(l,n,c) (((n) + ((l) << (c)) - (l)) >> (c))
+#define MEDIAN3(a,b,c)  ((a) > (b) ? ((b) > (c) ? (b) : ((a) > (c) ? (c) : (a))) \
+                                   : ((a) > (c) ? (a) : ((b) > (c) ? (c) : (b))))
+#define CHK_BIPOLAR_OFS(ofs) ((ofs < (2048 - 512)) || (ofs > (2048 + 512)))
+#define MAP(x, in_min, in_max, out_min,out_max) ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min) //TRASH
+
+
+#endif // MY_MATH_H_INCLUDED
